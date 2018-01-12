5e77b6e26df004e537aa0c8b7e58461046213d1d
Nicolas Jouanno <nicolas.jouanno@gmail.com>
2018-01-12 08:27:11 +0100

    getsitecontrol.js corr2

--- source/javascripts/global.js
+++ source/javascripts/global.js
@@ -130,10 +130,3 @@ $(function(){
   })
 
 })
-
-/*------
-GETSITECONTROL.js
-------*/
-(function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
-  t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
-})(window, document, '_gscq','script','//widgets.getsitecontrol.com/50678/script.js');
