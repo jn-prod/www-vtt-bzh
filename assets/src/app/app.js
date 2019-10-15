f485bc3b93340dbb9131c688d351b56268aac42c
Nicolas Jouanno <nicolas.jouanno@gmail.com>
2019-10-15 17:28:10 +0200

    Update app.js

--- assets/src/app/app.js
+++ assets/src/app/app.js
@@ -2,7 +2,6 @@
 import angular from 'angular';
 
 // services
-import eventService from './core/event/event.service';
 
 // components
 import eventList from './event-list/event-list.component';
@@ -10,6 +9,5 @@ import eventList from './event-list/event-list.component';
 // Define the `App` module
 export default angular.
   module('app', [
-    eventService.name,
     eventList.name
   ])
