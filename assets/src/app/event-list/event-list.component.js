bd55952d25e1f424d62877b5fab972f8609b8274
Nicolas Jouanno <nicolas.jouanno@gmail.com>
2019-10-15 08:46:10 +0200

    list display

--- assets/src/app/event-list/event-list.component.js
+++ assets/src/app/event-list/event-list.component.js
@@ -6,9 +6,13 @@ export default angular.
   module(eventList.name).
   component('eventList', {
     template: require('./event-list.template.html'),
-    controller: [ 'Event',
-      function EventListController(Event) {
-          this.events = Event.query();
-        }
-    ]
+    controller: ['$http',
+    	function EventListController($http) {
+	     var self = this;
+
+	      $http.get('api/events.json').then(function(response) {
+	        self.events = response.data;
+	      });
+	    }
+	  ]
   });
