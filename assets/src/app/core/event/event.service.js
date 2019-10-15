4ee0253458fd7928b03776d2fccb44d952ba395f
Nicolas Jouanno <nicolas.jouanno@gmail.com>
2019-10-15 17:28:50 +0200

    Update and rename assets/src/app/core/event/event.service.js to assets/src/app/core/services/event.service.js

--- assets/src/app/core/event/event.service.js
+++
@@ -1,8 +0,0 @@
-export default angular.module('core.event')
-  .factory('Event', function Event($http) {
-    return{
-        all: function(){
-            return $http.get("api/events.json);
-        }
-    }
-  });
