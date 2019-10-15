540826bf00113f1115e6c9f7940049e54f08cd38
Nicolas Jouanno <nicolas.jouanno@gmail.com>
2019-10-15 16:02:35 +0200

    Create event.service.js

---
+++ assets/src/app/core/event/event.service.js
@@ -0,0 +1,8 @@
+export default angular.module('core.event')
+  .factory('Event', function Event($http) {
+    return{
+        all: function(){
+            return $http.get("api/events.json);
+        }
+    }
+  });
