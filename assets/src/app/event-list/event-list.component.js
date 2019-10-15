9dcdc423db1075ecb6f09331c42f3af5b2ac1035
Nicolas Jouanno <nicolas.jouanno@gmail.com>
2019-10-15 14:00:47 +0200

    load more button

--- assets/src/app/event-list/event-list.component.js
+++ assets/src/app/event-list/event-list.component.js
@@ -8,11 +8,21 @@ export default angular.
     template: require('./event-list.template.html'),
     controller: ['$http',
     	function EventListController($http) {
-	     var self = this;
+	      var self = this;
+	      self.events = []
+	      self.paginator = 20
 
 	      $http.get('api/events.json').then(function(response) {
 	        self.events = response.data;
 	      });
+
+	      self.getEvents = function(){
+	      	if (self.events.length > 0) {
+						return self.events.slice(0,self.paginator)
+	      	} else {
+	      		return self.events
+	      	}
+	      }
 	    }
 	  ]
   });
