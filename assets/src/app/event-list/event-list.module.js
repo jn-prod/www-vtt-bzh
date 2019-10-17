468cd0b8bbad9ebae446bf8769b9f6277f2c4f8b
Nicolas Jouanno <nicolas.jouanno@gmail.com>
2019-10-17 16:18:49 +0200

    Update event-list.module.js

--- assets/src/app/event-list/event-list.module.js
+++ assets/src/app/event-list/event-list.module.js
@@ -1,3 +1,5 @@
 import eventService from '../core/event/event.service'
+import ngMaterial from 'angular-material'
+
 // Define the `eventList` module
-export default angular.module('eventList', [eventService.name]);
+export default angular.module('eventList', ['ngMaterial', eventService.name]);
