66aad7083202688e257852fc2238ce3c90f68b7e
Nicolas Jouanno <pro@nicolasjouanno.com>
2021-06-30 21:54:34 +0200

    add event cache

--- client/src/services/event-service.js
+++ client/src/services/event-service.js
@@ -1,23 +1,65 @@
 import axios from 'axios';
 import { dateFormat, dateNow } from '../utils/date';
 
-export default {
-  getEvents: async () => {
-    try {
-      const { data, status } = await axios.get('https://gleaypou6k.execute-api.eu-west-3.amazonaws.com/prod/events');
-      const { data: customEvents } = await axios.get('/api/events.json');
+const setEventsCache = (events) => {
+  const date = new Date();
+  const expireAt = date.setDate(date.getDate() + 1);
+  window.localStorage.setItem('events', JSON.stringify({ data: events, expireAt }));
+};
 
-      if (!data && status !== 200) return [];
+const getEventsFromCache = () => {
+  try {
+    const events = window.localStorage.getItem('events');
+    if (!events) return null;
+    const { data, expireAt } = JSON.parse(events);
+    if (new Date(expireAt) >= new Date()) return data;
+    return null;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
 
-      const { data: formatedData } = data;
+const getEventsFromApi = async () => {
+  try {
+    const { data, status } = await axios.get('https://gleaypou6k.execute-api.eu-west-3.amazonaws.com/prod/events');
+    const { data: customEvents } = await axios.get('/api/events.json');
+    // const customEvents = [];
+    if (!data && status !== 200) return [];
 
-      return [...formatedData, ...customEvents].filter((val) => {
-        if (!val.date) return false;
-        return dateFormat(val.date) >= dateNow;
-      }).sort((a, b) => dateFormat(a.date) - dateFormat(b.date));
-    } catch (e) {
-      console.error(e);
-      throw e;
+    const { data: formatedData } = data;
+
+    const events = [...formatedData, ...customEvents];
+
+    setEventsCache(events);
+
+    return events;
+  } catch (e) {
+    console.error(e);
+    throw (e);
+  }
+};
+
+const getEvents = async () => {
+  try {
+    let events;
+    const eventsFromCache = getEventsFromCache();
+    if (eventsFromCache) {
+      events = eventsFromCache;
+    } else {
+      events = await getEventsFromApi();
     }
-  },
+
+    return events.filter((val) => {
+      if (!val.date) return false;
+      return dateFormat(val.date) >= dateNow;
+    }).sort((a, b) => dateFormat(a.date) - dateFormat(b.date));
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+export default {
+  getEvents,
 };
