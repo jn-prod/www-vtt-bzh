3049b18966af208bb35dbcaa7c791a424068e5b8
Nicolas Jouanno <pro@nicolasjouanno.com>
2023-09-26 14:12:50 +0200

    fix: app .env

--- apps/calendar/vite.config.ts
+++ apps/calendar/vite.config.ts
@@ -5,8 +5,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  console.log(env.VITE_APP_API_BASE);
+  loadEnv(mode, process.cwd(), '');
+  // const env = loadEnv(mode, process.cwd(), '');
+  // console.log(env.VITE_APP_API_BASE);
   return {
     plugins: [vue(), vueJsx()],
     minify: true,
