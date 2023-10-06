840e81360e2f5d31032039a2c65530b66900528b
Nicolas Jouanno <pro@nicolasjouanno.com>
2023-10-06 13:00:13 +0200

    feat(analytics): disconnect mixpanel analytics

--- apps/calendar/src/main.ts
+++ apps/calendar/src/main.ts
@@ -3,10 +3,7 @@ import 'styles/dist/index.css';
 
 // Vue
 import 'styles';
-import { setup as setupAnalytics } from 'analytics';
 import { createApp } from 'vue';
 import App from '@/App.vue';
 
 createApp(App).mount('#calendar');
-
-setupAnalytics(import.meta.env.VITE_APP_MIXPANEL);
