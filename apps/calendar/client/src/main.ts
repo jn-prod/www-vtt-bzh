4789fded34120fcf40d560edf04d40787419dea7
Nicolas Jouanno <pro@nicolasjouanno.com>
2024-01-15 22:58:15 +0100

    feat(calendar): add isloading state

--- apps/calendar/client/src/main.ts
+++ apps/calendar/client/src/main.ts
@@ -4,8 +4,8 @@ import 'styles/dist/index.css';
 // Vue
 import 'styles';
 import { createApp } from 'vue';
-import SearchEventView from '@/views/SearchEventView.vue';
-import AddEventView from '@/views/AddEventView.vue';
+import SearchEventView from '@/views/SearchEventView/SearchEventView.vue';
+import AddEventView from '@/views/AddEventView/AddEventView.vue';
 
 createApp(SearchEventView).mount('#calendar');
 createApp(AddEventView).mount('#calendar-add');
