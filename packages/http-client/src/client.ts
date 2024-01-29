26975202cfbe24baf5d7a065d35c5d4c42b2c8c5
Nicolas Jouanno <pro@nicolasjouanno.com>
2024-01-29 13:37:35 +0100

    feat(client): use URL constructor

--- packages/http-client/src/client.ts
+++ packages/http-client/src/client.ts
@@ -12,21 +12,28 @@ const request = async <T>(
   options: RequestOptions = {}
 ): Promise<Result<T | ArrayBuffer | string>> => {
   return encaseResult<T | ArrayBuffer | string>(async () => {
-    let url = encodeURI(baseURL + uri);
+    // set clean url
+    const url = new URL(encodeURI([baseURL, uri.split('?')[0]].join('')));
+
+    // set search params
     if (options.params && Object.keys(options.params).length > 0) {
-      url += `?${new URLSearchParams(options.params).toString()}`;
+      url.search = new URLSearchParams(options.params).toString();
     }
-    const res = await fetch(new Request(url, options));
-    let response;
+
+    // fetch query
+    const response = await fetch(new Request(url.toString(), options));
+
+    // set result value
+    let result;
     if (options.responseType === 'arrayBuffer') {
-      response = await res.arrayBuffer();
-      return response;
+      result = await response.arrayBuffer();
+      return result;
     } else if (options.responseType === 'text') {
-      response = await res.text();
-      return response;
+      result = await response.text();
+      return result;
     } else {
-      response = await res.json();
-      return response as T;
+      result = await response.json();
+      return result as T;
     }
   });
 };
