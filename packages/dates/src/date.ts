b9a53c030707fc717ab23dc9a15e9294394f039a
Nicolas JOUANNO DANIEL <nicolas.jouanno@gmail.com>
2026-02-18 20:38:52 +0100

    Update date.ts

--- packages/dates/src/date.ts
+++ packages/dates/src/date.ts
@@ -1,11 +1,3 @@
-export const dateFormatToText = (date?: string = ''): string => {
-  try {
-      return new Intl.DateTimeFormat("fr-FR", { dateStyle: "medium", }).format(new Date(date));
-  } catch {
-      return '';
-  }
-};
-
 export const dateFormatToIsoString = (date: string): string => (date ? new Date(date.split("/").reverse().join("-")).toISOString() : '');
 
 export const getStringDate = (date: string | Date | undefined): string => {
