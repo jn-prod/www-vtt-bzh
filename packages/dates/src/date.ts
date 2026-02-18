876e2866a6f39e82c3e25e2d5fdeb76408485a41
Nicolas JOUANNO DANIEL <nicolas.jouanno@gmail.com>
2026-02-18 20:48:28 +0100

    Delete date.ts

--- packages/dates/src/date.ts
+++
@@ -1,5 +0,0 @@
-export const getStringDate = (date: string | Date | undefined): string => {
-  if (typeof date === 'string') return date;
-  else if (typeof date === 'object') return date.toDateString();
-  else return `${date}`;
-};
